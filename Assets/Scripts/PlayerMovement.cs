02b1dfb50cda175bf7a4379a56eafb2b1d633ddc
Birdem58 <huzeyfecelep@hotmail.com>
2023-08-12 19:05:55 +0300

    Player Movement değişiklikleri eklendi
    Eklenmesi gereken jump buffer ve cayote time

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,24 +8,29 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Transform groundCheck;
     public LayerMask groundLayer;
-
-    private float downForce = 5f;
+    [SerializeField] float apexTimer;
+    [SerializeField] float apexCounter;
+    [SerializeField] float fallSpeed = 5f;
     private float horizontal;
-    private float speed = 10f;
-    private float jumpPower = 10f;
+    [SerializeField] float speed = 10f;
+    [SerializeField] float speedBonus = 2f;
+    [SerializeField] float jumpPower = 10f;
     private bool isFacingRight = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        apexTimer -= Time.deltaTime;
+        WhenInAir();
 
-        if( !isFacingRight && horizontal > 0f)
+
+        if ( !isFacingRight && horizontal > 0f)
         {
             Flip();
         }
@@ -33,6 +38,8 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+
+        
     }
 
     public void Jump(InputAction.CallbackContext context )
@@ -45,6 +52,36 @@ public class PlayerMovement : MonoBehaviour
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+    }
+
+    public void WhenInAir()
+    {
+        
+        
+
+       
+        if (rb.velocity.y <= 1 && rb.velocity.y >= -1 && !isGrounded() )
+        {
+            rb.velocity = new Vector2(horizontal * speed * speedBonus, rb.velocity.y);
+            
+        }
+        else if (rb.velocity.y < -fallSpeed && !isGrounded())
+        {
+            rb.velocity = new Vector2(horizontal * speed, -fallSpeed + 1);
+        }
+        if(apexTimer <= 0) 
+        {
+            rb.gravityScale = 3;
+        }
+        else if(apexTimer > 0)
+        {
+            rb.gravityScale = 2;
+        }
+        if(isGrounded())
+        {
+            apexTimer = apexCounter;
+        }
+        
     }
 
     private bool isGrounded()
@@ -61,22 +98,6 @@ public class PlayerMovement : MonoBehaviour
     
     }
 
-    public void FreeFall()
-    {
-
-    }
-    public void Fall(InputAction.CallbackContext context)
-    {
-       if(isGrounded() == false && context.performed)
-        {
-            
-            rb.velocity = new Vector2(rb.velocity.x, - downForce);
-        }
-       if(isGrounded() && context.canceled)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0f);
-        }
-    }
 
 
     public void Move(InputAction.CallbackContext context)
